240deb46f243d119786d2a6a72ae4e4588bfb437
Mohammed-Abrar-Neeman <neman.abrar@gmail.com>
2024-05-17 20:52:14 +0530

    praticipants profile loading modal

--- src/app/participants/profile/page.tsx
+++ src/app/participants/profile/page.tsx
@@ -19,8 +19,11 @@ import Link from "next/link";
 // }
 
 export default function ParticipantsProfile() {
-  const [loading, setLoading] = useState<boolean>(true);
+  const [profileSubmit, setProfileSubmit] = useState<boolean>(false);
   const [isVerified, setIsVerified] = useState<boolean>(false);
+  const [isProfileSubmitted, setIsProfileSubmitted] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(true);
+  
   // const schema = yup.object({
   //   gender: yup.object({
   //     value: yup.string().required("Please select a gender"),
@@ -62,12 +65,15 @@ export default function ParticipantsProfile() {
       webcam: values.webcam.value,
     };
     try {
+      setProfileSubmit(true);
       const res = await axios.post(
         "/api/users/signup/participants/profileSubmit",
         profileData
       );
       console.log(res);
+      setProfileSubmit(false);
     } catch (error: any) {
+      setProfileSubmit(false);
       console.log("failed", error.message);
     }
     console.log(
@@ -97,13 +103,15 @@ export default function ParticipantsProfile() {
 
   const getUserDetails = async () => {
     const res = await axios.get("/api/users/me/participants");
+    console.log(res)
     setIsVerified(res.data.data.isVerified);
+    setIsProfileSubmitted(res.data.data.isProfileSubmitted);
     setLoading(false);
   };
 
   useEffect(() => {
     getUserDetails();
-  }, [isVerified]);
+  }, [isVerified,profileSubmit]);
 
   return (
     <>
@@ -171,7 +179,28 @@ export default function ParticipantsProfile() {
               </>
             ) : (
               <>
-                <div className="text-center  bg-blue-100 min-h-[160px] sm:p-6 p-4">
+              {isProfileSubmitted ? <>
+          <div
+      className="fixed inset-0 p-4 flex flex-wrap justify-center items-center w-full h-full  before:fixed before:inset-0 before:w-full before:h-full before:bg-[rgba(0,0,0,0.5)] overflow-auto font-[sans-serif]">
+      <div className="w-full max-w-lg bg-white shadow-lg rounded-md p-6 relative">
+        
+        <div className="my-8 text-center">
+          <svg xmlns="http://www.w3.org/2000/svg" className="w-16 shrink-0 fill-[#333] inline" viewBox="0 0 512 512">
+            <path
+              d="M383.841 171.838c-7.881-8.31-21.02-8.676-29.343-.775L221.987 296.732l-63.204-64.893c-8.005-8.213-21.13-8.393-29.35-.387-8.213 7.998-8.386 21.137-.388 29.35l77.492 79.561a20.687 20.687 0 0 0 14.869 6.275 20.744 20.744 0 0 0 14.288-5.694l147.373-139.762c8.316-7.888 8.668-21.027.774-29.344z"
+              data-original="#000000" />
+            <path
+              d="M256 0C114.84 0 0 114.84 0 256s114.84 256 256 256 256-114.84 256-256S397.16 0 256 0zm0 470.487c-118.265 0-214.487-96.214-214.487-214.487 0-118.265 96.221-214.487 214.487-214.487 118.272 0 214.487 96.221 214.487 214.487 0 118.272-96.215 214.487-214.487 214.487z"
+              data-original="#000000" />
+          </svg>
+          <h4 className="text-2xl text-[#333] font-semibold mt-6">Profile Successfully Submitted!</h4>
+          <p className="text-sm text-black mt-4">We will review your profile and get back to you with matching interviews.</p>
+          <p className="text-sm text-black mt-4">Thank you!</p>
+        </div>
+      </div>
+    </div>
+         </>:<>
+         <div className="text-center  bg-blue-100 min-h-[160px] sm:p-6 p-4">
                   <h4 className="md:text-4xl text-3xl font-extrabold mb-6">
                     Please fill your details.
                   </h4>
@@ -790,10 +819,34 @@ export default function ParticipantsProfile() {
                     </form>
                   </div>
                 </div>
+         </>}
+                
               </>
             )}
           </>
         )}
+        {profileSubmit && <>
+          <div className="fixed inset-0 p-4 flex flex-wrap justify-center items-center w-full h-full  font-[sans-serif]">
+              <button
+                type="button"
+                className="px-6 py-2.5 rounded text-white text-sm tracking-wider font-semibold border-none outline-none bg-[#1553A4] hover:bg-[#1553A4] active:[#1553A4]"
+              >
+                Submitting, Please wait...
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="18px"
+                  fill="#fff"
+                  className="ml-2 inline animate-spin"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    d="M12 22c5.421 0 10-4.579 10-10h-2c0 4.337-3.663 8-8 8s-8-3.663-8-8c0-4.336 3.663-8 8-8V2C6.579 2 2 6.58 2 12c0 5.421 4.579 10 10 10z"
+                    data-original="#000000"
+                  />
+                </svg>
+              </button>
+            </div>
+         </>}
       </div>
     </>
   );
